2b0b64381d2e8cc72abea7ac3b680537ff5ec141
malachi-hale <malachi.d.hale@gmail.com>
2021-06-23 10:45:54 -0500

    This is the final version

--- select_exercises.sql
+++ select_exercises.sql
@@ -2,6 +2,31 @@ USE albums_db;
 SELECT * 
 	FROM albums;
 SELECT 
-	DISTINCT artist,
-			id
+	DISTINCT artist
 	FROM albums;
+DESCRIBE albums;
+SELECT 
+	MAX(release_date) 
+	FROM albums;
+SELECT 
+	MIN(release_date) 
+	FROM albums;
+SELECT name
+	FROM albums 
+	WHERE artist = 'Pink Floyd';
+SELECT release_date 
+	FROM albums 
+	WHERE name = 'Sgt. Pepper\'s Lonely Hearts Club Band';
+SELECT genre 
+	FROM albums 
+	WHERE name = 'Nevermind';
+SELECT * 
+	FROM albums 
+	WHERE release_date BETWEEN 1990 and 1999; 
+SELECT * 
+	FROM albums 
+	WHERE sales < 20;
+SELECT * 
+	FROM albums 
+	WHERE genre = 'Rock';
+	
