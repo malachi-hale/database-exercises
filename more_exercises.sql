4dd765ee1063e189ca982b2d28eb70307ad46609
malachi-hale <malachi.d.hale@gmail.com>
2021-07-03 17:58:10 -0500

    More exercises update

--- more_exercises.sql
+++ more_exercises.sql
@@ -1,5 +1,9 @@
+
 USE world;
 show tables;
+
+SELECT * 
+	FROM country;
 
 SELECT * 
 	FROM city;
@@ -35,3 +39,72 @@ SELECT region, AVG(LifeExpectancy) as life_expectancy
 	FROM country
 	GROUP BY region
 	ORDER BY life_expectancy ASC;
+	
+SELECT Name, LocalName
+	FROM country 
+		WHERE Name != LocalName;
+
+SELECT COUNT(*) 
+	FROM country
+		WHERE LifeExpectancy < 70;
+		
+SELECT district
+	FROM city 
+		WHERE city = 'New York';
+
+#Sakila Database
+
+use sakila;
+show tables;
+
+SELECT * 
+	FROM actor;
+SELECT *
+	FROM actor_info;
+SELECT *
+	FROM film_actor;
+SELECT *
+	FROM country;
+SELECT * 
+	FROM address;
+SELECT * 
+	FROM staff;
+SELECT * 
+	FROM film_list;
+SELECT *
+	FROM film_actor;
+
+#1	
+SELECT lower(CONCAT(first_name, ' ', last_name))
+	FROM actor;
+
+#3
+SELECT *
+	FROM actor 
+		WHERE last_name LIKE '%gen%';
+		
+#4 
+SELECT * 
+	FROM actor
+		WHERE last_name LIKE '%li%'
+		ORDER BY last_name, first_name;
+		
+#5 
+SELECT country_id, country
+	FROM country
+		WHERE country IN ('Afghanistan', 'Bangladesh', 'China');
+		
+#6 
+SELECT DISTINCT last_name as last_name, COUNT(last_name) as count
+	FROM actor
+	GROUP BY last_name
+	ORDER BY count DESC;
+	
+#7
+SELECT DISTINCT last_name, COUNT(last_name) as count
+	FROM actor
+	GROUP BY last_name
+	HAVING count > 1
+	ORDER BY count DESC;
+	
+
